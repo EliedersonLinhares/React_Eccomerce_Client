bb0faaa36ed295b6cc618066cee56eb46037e17c
EliedersonLinhares <eliederson250@gmail.com>
2021-07-09 00:38:05 -0300

    Password Update

---
+++ src/pages/user/Wishlist.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import UserNav from '../../Components/nav/UserNav'
+
+const Wishlist = () => (
+	<div className='container-fluid'>
+		<div className='row'>
+			<div className='col-md-2'>
+				<UserNav />
+			</div>
+			<div className='col'>user Wishlist page</div>
+		</div>
+	</div>
+)
+
+export default Wishlist
