844eb78277c8d1cb728315c2da688570d9797ce5
EliedersonLinhares <eliederson250@gmail.com>
2021-07-15 13:27:07 -0300

    Category Filter refactoring

--- src/pages/admin/category/CategoryCreate.js
+++ src/pages/admin/category/CategoryCreate.js
@@ -10,6 +10,7 @@ import {
 import { Link } from 'react-router-dom'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import CategoryForm from '../../../Components/forms/CategoryForm'
+import LocalSearch from '../../../Components/forms/LocalSearch'
 
 const CategoryCreate = () => {
 	const { user } = useSelector((state) => ({ ...state }))
@@ -67,12 +68,6 @@ const CategoryCreate = () => {
 		}
 	}
 
-	//step 3
-	const handleSearchChange = (e) => {
-		e.preventDefault()
-		setKeyword(e.target.value.toLowerCase())
-	}
-
 	//step 4
 	const searched = (keyword) => (c) => c.name.toLowerCase().includes(keyword)
 
@@ -93,15 +88,10 @@ const CategoryCreate = () => {
 						name={name}
 						setName={setName}
 					/>
-					{/** step 2  */}
-					<input
-						type='search'
-						placeholder='Filter'
-						value={keyword}
-						onChange={handleSearchChange}
-						className='form-control mb-4'
-					/>
 					<hr />
+					{/** step 2 and 3 */}
+					<LocalSearch keyword={keyword} setKeyword={setKeyword} />
+
 					{/** step 5 */}
 					{categories.filter(searched(keyword)).map((c) => (
 						<div className='alert alert-secondary' key={c._id}>
