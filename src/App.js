161d6016793d0f4dc6aab4222024579f8a5a56a0
EliedersonLinhares <eliederson250@gmail.com>
2021-07-20 11:53:56 -0300

    Start with Product

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import CategoryCreate from './pages/admin/category/CategoryCreate'
 import CategoryUpdate from './pages/admin/category/CategoryUpdate'
 import SubCreate from './pages/admin/sub/SubCreate'
 import SubUpdate from './pages/admin/sub/SubUpdate'
+import ProductCreate from './pages/admin/product/ProductCreate'
 
 import { auth } from './firebase'
 import { useDispatch } from 'react-redux'
@@ -74,6 +75,7 @@ const App = () => {
 					component={CategoryUpdate}
 				/>
 				<AdminRoute exact path='/admin/sub/:slug' component={SubUpdate} />
+				<AdminRoute exact path='/admin/product' component={ProductCreate} />
 			</Switch>
 		</>
 	)
