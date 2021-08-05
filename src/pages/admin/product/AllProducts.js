3bc367a65f79962d1ece7dd3b9ea33dc943c1a58
EliedersonLinhares <eliederson250@gmail.com>
2021-08-05 12:23:43 -0300

    Delete product function

--- src/pages/admin/product/AllProducts.js
+++ src/pages/admin/product/AllProducts.js
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from 'react'
 import AdminNav from '../../../Components/nav/AdminNav'
 import { getProductsbyCount } from '../../../functions/product'
 import AdminProductCard from '../../../Components/cards/AdminProductCard'
+import { removeProduct } from '../../../functions/product'
+import { useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
 
 const AllProducts = () => {
 	const [products, setProducts] = useState([])
 	const [loading, setLoading] = useState(false)
+	//redux
+	const { user } = useSelector((state) => ({ ...state }))
 
 	useEffect(() => {
 		loadAllProducts()
@@ -24,6 +29,21 @@ const AllProducts = () => {
 			})
 	}
 
+	const handleRemove = (slug) => {
+		let answer = window.confirm('Confirm delete?')
+		if (answer) {
+			removeProduct(slug, user.token)
+				.then((res) => {
+					loadAllProducts()
+					toast.error(`${res.data.title} is deleted`)
+				})
+				.catch((err) => {
+					console.log(err)
+					if (err.response.status === 400) toast.error(err.response.data)
+				})
+		}
+	}
+
 	return (
 		<div className='container-fluid'>
 			<div className='row'>
@@ -40,7 +60,10 @@ const AllProducts = () => {
 					<div className='row'>
 						{products.map((product) => (
 							<div key={product._id} className='col-md-4 pb-3'>
-								<AdminProductCard product={product} />
+								<AdminProductCard
+									product={product}
+									handleRemove={handleRemove}
+								/>
 							</div>
 						))}
 					</div>
