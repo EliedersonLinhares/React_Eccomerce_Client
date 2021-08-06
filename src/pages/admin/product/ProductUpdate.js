c0fecb934add450f02af91a431462c4d26cb30ea
EliedersonLinhares <eliederson250@gmail.com>
2021-08-06 13:12:00 -0300

    Get product on update page

--- src/pages/admin/product/ProductUpdate.js
+++ src/pages/admin/product/ProductUpdate.js
@@ -2,17 +2,47 @@ import React, { useState, useEffect } from 'react'
 import AdminNav from '../../../Components/nav/AdminNav'
 import { toast } from 'react-toastify'
 import { useSelector } from 'react-redux'
-import { createProduct } from '../../../functions/product'
-import ProductCreateForm from '../../../Components/forms/ProductCreateForm'
+import { getProduct } from '../../../functions/product'
+
 import FileUpload from '../../../Components/forms/FileUpload'
 import { LoadingOutlined } from '@ant-design/icons'
 
 import { getCategories, getCategorySubs } from '../../../functions/category'
 
-const ProductUpdate = () => {
+const initialState = {
+	title: '',
+	description: '',
+	price: '',
+	categories: [],
+	category: '',
+	subs: [],
+	shipping: '',
+	quantity: '',
+	images: [],
+	colors: ['Black', 'Brown', 'Silver', 'White', 'Blue'],
+	brands: ['Apple', 'Samsung', 'Microsoft', 'Lenovo', 'ASUS'],
+	color: '',
+	brand: '',
+}
+
+const ProductUpdate = ({ match }) => {
+	//state
+	const [values, setValues] = useState(initialState)
+
 	// redux
 	const { user } = useSelector((state) => ({ ...state }))
 
+	const { slug } = match.params
+
+	useEffect(() => {
+		loadProduct()
+	}, [])
+
+	const loadProduct = () => {
+		getProduct(slug).then((p) => {
+			setValues({ ...values, ...p.data })
+		})
+	}
 	return (
 		<div className='container-fluid'>
 			<div className='row'>
