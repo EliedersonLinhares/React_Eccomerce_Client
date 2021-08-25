2fb1c2500ad3fc9efb608f5c499b35cb6f2d866c
EliedersonLinhares <eliederson250@gmail.com>
2021-08-25 11:30:09 -0300

    Loading Card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { getProductsbyCount } from '../functions/product'
 import ProductCard from '../Components/cards/ProductCard'
 import Jumbotron from '../Components/cards/Jumbotron'
+import LoadingCard from '../Components/cards/LoadingCard'
 
 const Home = () => {
 	const [products, setProducts] = useState([])
@@ -26,13 +27,17 @@ const Home = () => {
 			</div>
 
 			<div className='container'>
-				<div className='row'>
-					{products.map((product) => (
-						<div key={product._id} className='col-md-4'>
-							<ProductCard product={product} />
-						</div>
-					))}
-				</div>
+				{loading ? (
+					<LoadingCard count={3} />
+				) : (
+					<div className='row'>
+						{products.map((product) => (
+							<div key={product._id} className='col-md-4'>
+								<ProductCard product={product} />
+							</div>
+						))}
+					</div>
+				)}
 			</div>
 		</>
 	)
