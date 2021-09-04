6cc8e1b2f09b84f782341fb151d6ba21b463a075
EliedersonLinhares <eliederson250@gmail.com>
2021-09-04 16:17:39 -0300

    API Request with Rating and Show users exiting rating

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { getProduct } from '../functions/product'
+import { getProduct, productStar } from '../functions/product'
 import SingleProduct from '../Components/cards/SingleProduct'
+import { useSelector } from 'react-redux'
 
 const Product = ({ match }) => {
 	const [product, setProduct] = useState({})
+	const [star, setStar] = useState(0)
+	//redux
+	const { user } = useSelector((state) => ({ ...state }))
 
 	const { slug } = match.params
 
@@ -11,14 +15,34 @@ const Product = ({ match }) => {
 		loadSingleProduct()
 	}, [slug])
 
+	useEffect(() => {
+		if (product.ratings && user) {
+			let existingRatingObject = product.ratings.find(
+				(ele) => ele.postedBy.toString() === user._id.toString()
+			)
+			existingRatingObject && setStar(existingRatingObject.star) //current user star
+		}
+	})
+
 	const loadSingleProduct = () => {
 		getProduct(slug).then((res) => setProduct(res.data))
+	}
+
+	const onStarClick = (newRating, name) => {
+		setStar(newRating)
+		productStar(name, newRating, user.token).then((res) => {
+			loadSingleProduct() //if want update product in real time
+		})
 	}
 
 	return (
 		<div className='container-fluid'>
 			<div className='row pt-4'>
-				<SingleProduct product={product} />
+				<SingleProduct
+					product={product}
+					onStarClick={onStarClick}
+					star={star}
+				/>
 			</div>
 			<div className='row'>
 				<div className='col text-center pt-5 pb-5'>
